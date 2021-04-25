d630a28225ec09953f8661ce395f671072308073
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-04-25 21:21:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function moveDodgerRight() {
   }
 }
 
-// document.addEventListener("keydown", function(e) {
-//   if (e.key === "ArrowLeft") {
-//     moveDodgerLeft();
-//   } else if (e.key === "ArrowRight") {
-//     moveDodgerRight();
-//   }
-// });
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
