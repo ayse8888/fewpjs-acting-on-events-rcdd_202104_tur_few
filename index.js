aa379a6d44a650c7e3c2448c221f72499c3406b0
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-04-25 21:20:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  console.log(dodger.style.right);
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  // console.log(dodger.style.left);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
  
-  if (right > 179) {
-    dodger.style.right = `${right + 1}px`;
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
 }
 
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  } else if (e.key === "ArrowRight") {
-    moveDodgerRight();
-  }
-});
+// document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft") {
+//     moveDodgerLeft();
+//   } else if (e.key === "ArrowRight") {
+//     moveDodgerRight();
+//   }
+// });
